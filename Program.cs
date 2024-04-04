abbc346fa6bdaefacd404f5b030e4f4273f0542c
ReynoldOcampo <raocampo005@gmail.com>
2024-04-04 12:09:38 -0600

    define cell state, GameBoard

--- Program.cs
+++ Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿using System;
+
+public enum CellState //define cell on the gameboard
+{
+    Empty;
+    PlayerOne;
+    PlayerTwo;
+}
+
+public class GameBoard //gameboard
+{
+    private const int Rows = 6
+    private const int Cols = 7
+    private CellState[,] board;
+}
+
+public GameBoard()
+{
+    GameBoard = new CellState[Rows + 1, Cols + 1];
+    ClearBoard();
+}
